ca2c6b5124f9227a9cfcba8417c17ce44771b132
Maple <xxx871022@gmail.com>
2026-03-05 16:32:42 +0800

    chore: 移除 EF Core 資料庫層，還原為基礎 API 架構

--- API/Program.cs
+++ API/Program.cs
@@ -1,41 +1,27 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using API.Data;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-// MySQL
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(
-        connectionString,
-        new MySqlServerVersion(new Version(8, 0, 21))
-    )
-);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Configure the HTTP request pipeline. 課程說這邊想先移除欸
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
-app.UseHttpsRedirection();
+// 不是https的話就不需要重定向了
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// 認證和授權的部分課程也說先移除
+// app.UseAuthorization();
 
 app.MapControllers();
 
